ce4cf011fb833bb0d692933fe1bf9fc8874bd14d
erictweigle <erictweigle@gmail.com>
2015-06-02 23:30:04 -0700

    subnav dev & styling

--- Theme/functions.php
+++ Theme/functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /* 
 Theme Name: WEB170 Project Site
 Author: Eric Weigle
@@ -6,9 +7,12 @@ Author URI : http://www.pootacheeta.com/wordpress
 Descript: This the the theme for the WEB170 class
 Version : 1.0
 */
+
 // Register My Menus
 register_nav_menus(array(
 'main-menu' => __( 'Main' ),
 ));
 //
+
+
 ?>
--- Theme/index.php
+++ Theme/index.php
@@ -42,7 +42,7 @@
 					<li><a href="#">CONTACT</a></li>
 				</ul> -->
                 
-                <?php wp_nav_menu( array( 'theme_location' => 'main-menu','container_id' => 'nav-main',) ); ?>
+                <?php wp_nav_menu( array( 'theme_location' => 'main-menu','container' => 'nav',) ); ?>
                 
 			</nav> <!-- END NAV -->
 			<div id="middle"> <!-- BEGIN MIDDLE -->
@@ -59,7 +59,8 @@
                 </div> <!-- END CONTENT -->
                 
 				<div id="right-sidebar">
-                    <div id="sidebar-contact">
+                    
+                    <!--<div id="sidebar-contact">
                         <h3>Contact</h3>
                         <p>Hi there, you should give me jobs and money. I want all of your money please.</p>
                         <a href="contact.php">Contact Danielle &raquo;</a>
@@ -70,7 +71,38 @@
                         <a href="images/resume.pdf">View Resume &raquo;</a>
                         <br />
                         <a href="images/resume.pdf">Download Resume &raquo;</a>
-                    </div>
+                    </div>-->
+                    
+                    <?php if (is_page()) : ?>
+                    
+                    <h2><?php echo get_the_title($post->post_parent); //get the gateway page title ?></h2>
+                    <ul>
+                                                
+                    <?php
+
+                    if ($post->post_parent) {
+
+                        wp_list_pages( array( 'child_of' => $post->post_parent, 'title_li' => '', ));
+                    
+                    } else {
+                        wp_list_pages( array( 'child_of' => $post->ID, 'title_li' => '', ));
+
+                    }
+                        
+                    ?>
+                    </ul>
+                    
+                    <?php endif; ?>
+                    <?php if (!(is_page())) : ?>
+                    
+                    <h2>Blog</h2>
+                    <ul>                    
+                    <?php wp_list_categories( array('title_li' => '')); ?>
+                    </ul>
+                    
+                    <?php endif; ?>
+                    
+                    
                 </div>
 				
 			</div> <!-- END MIDDLE -->
