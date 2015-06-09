3153d0cd9ed69562d1c8279dd16a57113075dde8
erictweigle <erictweigle@gmail.com>
2015-06-09 00:18:41 -0700

    Pull-quote with custom fields

--- Theme/sidebar.php
+++ Theme/sidebar.php
@@ -1,45 +1,56 @@
                 <div id="right-sidebar"> <!-- START RIGHT SIDEBAR -->
-                    
-                    <!--<div id="sidebar-contact">
-                        <h3>Contact</h3>
-                        <p>Hi there, you should give me jobs and money. I want all of your money please.</p>
-                        <a href="contact.php">Contact Danielle &raquo;</a>
-                    </div>
-                    <div id="sidebar-resume">
-                        <h3>Resume</h3>
-                        <p>Hi there, are you wondering if you should give me all your money? Here's my resume!</p>
-                        <a href="images/resume.pdf">View Resume &raquo;</a>
-                        <br />
-                        <a href="images/resume.pdf">Download Resume &raquo;</a>
-                    </div>-->
-                    
-                    <?php if (is_page()) : ?>
-                    
-                    <h2><?php echo get_the_title($post->post_parent); //get the gateway page title ?></h2>
-                    <ul>
-                                                
-                    <?php
 
-                    if ($post->post_parent) {
+                    <!-- BEGIN SUBNAVIGATION -->
 
-                        wp_list_pages( array( 'child_of' => $post->post_parent, 'title_li' => '', ));
+                    <div id="sidebar-subnav">
                     
-                    } else {
-                        wp_list_pages( array( 'child_of' => $post->ID, 'title_li' => '', ));
-
-                    }
+                        <!--<div id="sidebar-contact">
+                            <h3>Contact</h3>
+                            <p>Hi there, you should give me jobs and money. I want all of your money please.</p>
+                            <a href="contact.php">Contact Danielle &raquo;</a>
+                        </div>
+                        <div id="sidebar-resume">
+                            <h3>Resume</h3>
+                            <p>Hi there, are you wondering if you should give me all your money? Here's my resume!</p>
+                            <a href="images/resume.pdf">View Resume &raquo;</a>
+                            <br />
+                            <a href="images/resume.pdf">Download Resume &raquo;</a>
+                        </div>-->
                         
-                    ?>
-                    </ul>
-                    
+                        <?php if (is_page()) : ?>
+                        
+                        <h2><?php echo get_the_title($post->post_parent); //get the gateway page title ?></h2>
+                        
+                        <ul>
+                        <?php
+                        if ($post->post_parent) {
+                            wp_list_pages( array( 'child_of' => $post->post_parent, 'title_li' => '', ));
+                        } else {
+                            wp_list_pages( array( 'child_of' => $post->ID, 'title_li' => '', ));
+                        }
+                        ?>
+                        </ul>
+                        
+                        <?php endif; ?>
+                        <?php if (!(is_page())) : ?>
+                        
+                        <h2>Blog</h2>
+                        <ul><?php wp_list_categories( array('title_li' => '')); ?></ul>
+                        
+                        <?php endif; ?>
+
+                    </div>
+                    <!-- END SUBNAVIGATION -->
+
+                    <!-- BEGIN QUOTE -->
+                    <div id="sidebar-quote">
+
+                    <?php if (get_post_meta($post->ID, 'Quote', true)) : ?>
+                    <blockquote><?php echo get_post_meta($post->ID, 'Quote', true) ?></blockquote>
                     <?php endif; ?>
-                    <?php if (!(is_page())) : ?>
-                    
-                    <h2>Blog</h2>
-                    <ul>                    
-                    <?php wp_list_categories( array('title_li' => '')); ?>
-                    </ul>
-                    
-                    <?php endif; ?>                    
-                    
+
+                    </div>
+                    <!-- END QUOTE -->
+
+
                 </div> <!-- END RIGHT SIDEBAR -->
